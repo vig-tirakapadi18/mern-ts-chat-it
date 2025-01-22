c419298f8c54c4b5f9ab2ea7223c07cee0a4c425
Vighnesh Tirakapadi <vighnesh.tirakapadi@deeporion.com>
2025-01-22 20:58:28 +0530

    creating Home Page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { FC } from "react";
+import { useChatStore } from "../store/useChatStore";
+import Sidebar from "../components/Sidebar";
+import NoChatSelected from "../components/NoChatSelected";
+import ChatContainer from "../components/ChatContainer";
 
-const Home = () => {
-  return <div>Home</div>;
+const Home: FC = (): React.JSX.Element => {
+  const { selectedUser } = useChatStore();
+
+  return (
+    <section className="h-screen bg-base-200">
+      <div className="flex items-center justify-center pt-20 px-4">
+        <div className="bg-base-100 rounded-lg shadow-xl w-full max-w-6xl h-[calc(100vh-8rem)]">
+          <div className="flex h-full rounded-lg overflow-hidden">
+            <Sidebar />
+
+            {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
---
+++ client/src/store/useChatStore.ts
@@ -0,0 +1,60 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios";
+import { AxiosError } from "axios";
+import toast from "react-hot-toast";
+
+// interface IMessage {}
+
+interface IChatStore {
+  //   messages: IMessage[];
+  //   users: IUser[];
+  //   selectedUser: IUser | null;
+  messages: any;
+  users: any;
+  selectedUser: any;
+  isUsersLoading: boolean;
+  isMessagesLoading: boolean;
+  getUsers: () => void;
+}
+
+export const useChatStore = create<IChatStore>((set) => ({
+  messages: [],
+  users: [],
+  selectedUser: null,
+  isUsersLoading: false,
+  isMessagesLoading: false,
+
+  getUsers: async () => {
+    set({ isUsersLoading: true });
+    try {
+      const response = await axiosInstance.get("/messages/users");
+      set({ users: response.data.users });
+    } catch (error: unknown) {
+      console.log("GET USERS", error);
+      if (error instanceof AxiosError) {
+        toast.error(error.response?.data.message);
+      } else {
+        toast.error("Error fetching users!");
+      }
+    } finally {
+      set({ isUsersLoading: false });
+    }
+  },
+
+  getMessages: async (userId: string) => {
+    set({ isMessagesLoading: true });
+    try {
+      const response = await axiosInstance.get(`/messages/${userId}`);
+      set({ messages: response.data.messages });
+    } catch (error: unknown) {
+      console.log("GET MESSAGES", error);
+      if (error instanceof AxiosError) {
+        toast.error(error.response?.data.message);
+      } else {
+        toast.error("Error fetching messages!");
+      }
+    } finally {
+      set({ isMessagesLoading: false });
+    }
+  },
+}));
