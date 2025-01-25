1e97e5f333abc6a88170783b832f520709589a35
Vighnesh Tirakapadi <vighnesh.tirakapadi@deeporion.com>
2025-01-25 16:28:43 +0530

    added messages and completed chat window

--- client/src/components/ChatHeader.tsx
+++ client/src/components/ChatHeader.tsx
@@ -8,19 +8,25 @@ const ChatHeader: FC = (): React.JSX.Element => {
   const { onlineUsers } = useAuthStore();
 
   return (
-    <header className="py-2 px-4 border-b-[0.5px] border-gray-600 shadow-lg">
+    <header className="py-2 px-4 border-b-[0.5px] border-gray-600 shadow-lg sticky top-0 z-10 bg-gray-800">
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-3">
           <div className="avatar">
             <div className="size-10 rounded-full relative">
-              <img src={selectedUser.profilePic} alt={selectedUser.fullName} className="" />
+              <img
+                src={selectedUser?.profilePic}
+                alt={selectedUser?.name}
+                className=""
+              />
             </div>
           </div>
 
           <div className="">
-            <h3 className="font-semibold text-xl">{selectedUser.name}</h3>
+            <h3 className="font-semibold text-xl">{selectedUser?.name}</h3>
             <p className="text-sm text-gray-300 font-light mt-1">
-              {onlineUsers.includes(selectedUser._id) ? "Online" : "Offline"}
+              {onlineUsers.includes(selectedUser?._id as any)
+                ? "Online"
+                : "Offline"}
             </p>
           </div>
         </div>
