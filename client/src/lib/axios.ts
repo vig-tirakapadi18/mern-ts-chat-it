bf3ae76a0c9db9c1ed6fa46da26b679c06c51918
Vighnesh Vijay Tirakapadi <106863618+vig-tirakapadi18@users.noreply.github.com>
2025-05-29 17:15:12 +0530

    upadetd axios

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -2,5 +2,5 @@ import axios, { AxiosInstance } from "axios";
 
 export const axiosInstance: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL as string,
-  // withCredentials: true,
+  withCredentials: true,
 });
