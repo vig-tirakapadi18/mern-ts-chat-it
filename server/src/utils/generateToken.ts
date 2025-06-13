a4c2890b811ae33f704b80a17caa8a3b7fda3b94
Vighnesh Tirakapadi <vighnesh.tirakapadi@deeporion.com>
2025-06-13 19:41:27 +0530

    changed cookie environment

--- server/src/utils/generateToken.ts
+++ server/src/utils/generateToken.ts
@@ -10,7 +10,7 @@ export const generateToken = (payload: JwtPayload, res: Response) => {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     sameSite: "strict",
-    secure: process.env.NODE_ENV !== "development",
+    secure: process.env.NODE_ENV !== "production",
   });
 
   return token;
